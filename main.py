5645cdfb0929beb79781a223e336f60c8b9a7120
Kaushal Patil <68244535+kaushal1014@users.noreply.github.com>
2020-09-11 01:34:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,12 +25,39 @@ def follow():
     PostNotification = driver.find_element_by_class_name('mt3GC')
     PostNotification.click()
     time.sleep(5)
-    for i in range(3):
-        for j in range(6):
-            follow = driver.find_element_by_xpath("//button[text()='Follow']")
-            follow.click()
-            time.sleep(2)
+        for i in range(2):
+            time.sleep(3)
+            for j in range(1):
+                time.sleep(3)
+                follow1 = driver.find_element_by_xpath(
+                    '/html/body/div[1]/section/main/section/div[3]/div[2]/div[2]/div/div/div/div[1]/div[3]/button')
+                follow1.click()
+                time.sleep(1)
+
+                follow2 = driver.find_element_by_xpath(
+                    '/html/body/div[1]/section/main/section/div[3]/div[2]/div[2]/div/div/div/div[2]/div[3]/button')
+                follow2.click()
+                time.sleep(1)
+
+                follow3 = driver.find_element_by_xpath(
+                    '/html/body/div[1]/section/main/section/div[3]/div[2]/div[2]/div/div/div/div[3]/div[3]/button')
+                follow3.click()
+                time.sleep(1)
+
+                follow4 = driver.find_element_by_xpath(
+                    '/html/body/div[1]/section/main/section/div[3]/div[2]/div[2]/div/div/div/div[4]/div[3]/button'
+                )
+                follow4.click()
+                time.sleep(1)
+
+                follow5 = driver.find_element_by_xpath(
+                    '/html/body/div[1]/section/main/section/div[3]/div[2]/div[2]/div/div/div/div[5]/div[3]/button'
+                )
+                follow5.click()
+                time.sleep(1)
+
             driver.refresh()
-    driver.close()
+        driver.close()
+    
     
 follow()
